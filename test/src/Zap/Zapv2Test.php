7f35ca93239b763e8361fc1485712375ff2b4afa
Yuki <yukisov@gmail.com>
2014-10-12 13:19:32 +0900

    Add PHPUnit and some tests

---
+++ test/src/Zap/Zapv2Test.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Zap\Test;
+
+class Zapv2Test extends \PHPUnit_Framework_TestCase{
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->proxy = "tcp://localhost:8090";
+        $this->target_url = "https://www.owasp.org/index.php/OWASP_Zed_Attack_Proxy_Project";
+    }
+
+    /**
+     * @test
+     */
+    public function testVersion()
+    {
+        $zap = new \Zap\Zapv2($this->proxy);
+        $version = @$zap->core->version();
+        $this->assertSame("2.3.1", $version);
+    }
+
+    /**
+     * @test
+     */
+    public function testStatus()
+    {
+        $zap = new \Zap\Zapv2($this->proxy);
+        $res = $zap->statusCode($this->target_url);
+        $this->assertSame("200", $res);
+    }
+} 
